ec2864a52d7278facfc2ec0bc81b253999d03b48
AdministratorProgrammer <komarovaleksandr8228@gmail.com>
2024-06-24 02:43:56 +0400

    Update 24.06.2024

---
+++ telegram/main_portal/src/scripts/main.js
@@ -0,0 +1,11 @@
+// Init TWA
+Telegram.WebApp.ready();
+
+// Event occurs whenever theme settings are changed in the user's Telegram app (including switching to night mode).
+Telegram.WebApp.onEvent('themeChanged', function() {
+    document.documentElement.className = Telegram.WebApp.colorScheme;
+});
+
+function testFunc() {
+	alert("Tap tap!");
+}
